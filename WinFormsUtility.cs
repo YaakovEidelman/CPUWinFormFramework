2453946f0a0e01147f50f735a8474a0f7393ba63
yaakoveidelman <yeidelman@eidelmans.com>
2024-07-14 17:10:43 -0400

    initial_commit

---
+++ WinFormsUtility.cs
@@ -0,0 +1,46 @@
+﻿using System.Data;
+
+namespace CPUWinFormFramwork
+{
+    public class WinFormsUtility
+    {
+        public static void SetListBinding(ComboBox lst, DataTable dtDisplay, DataTable dtBinding, string valuemember)
+        {
+            lst.DataSource = dtDisplay;
+            lst.ValueMember = valuemember;
+            lst.DisplayMember = lst.Name.Substring(3);
+            lst.DataBindings.Add("SelectedValue", dtBinding, lst.ValueMember, false, DataSourceUpdateMode.OnPropertyChanged);
+        }
+        public static void SetControlBinding(Control ctrl, DataTable dt)
+        {
+            string propertyname = "";
+            string controlname = ctrl.Name.ToLower();
+            string controltype = controlname.Substring(0, 3);
+            string columnname = controlname.Substring(3);
+
+            switch (controltype)
+            {
+                case "txt":
+                case "lbl":
+                    propertyname = "Text";
+                    break;
+                case "dtp":
+                    propertyname = "Value";
+                    break;
+            }
+
+            if (propertyname != "" && columnname != "")
+            {
+                ctrl.DataBindings.Add(propertyname, dt, columnname, true, DataSourceUpdateMode.OnPropertyChanged);
+            }
+        }
+
+        public static void FormatGridSearchResults(DataGridView grid)
+        {
+            grid.AllowUserToAddRows = false;
+            grid.ReadOnly = true;
+            grid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            grid.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+    }
+}
