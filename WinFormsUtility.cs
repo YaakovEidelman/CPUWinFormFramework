de8fe9949f39fc59176c836467a1c265d5fb10ae
yaakoveidelman <yeidelman@eidelmans.com>
2024-08-26 18:42:13 -0400

    update for session 25.6.1

--- WinFormsUtility.cs
+++ WinFormsUtility.cs
@@ -1,4 +1,6 @@
 ﻿using System.Data;
+using System.DirectoryServices.ActiveDirectory;
+using System.Xml.Linq;
 
 namespace CPUWinFormFramework
 {
@@ -35,12 +37,111 @@ namespace CPUWinFormFramework
             }
         }
 
-        public static void FormatGridSearchResults(DataGridView grid)
+        public static void FormatGridSearchResults(DataGridView grid, string tablename)
         {
             grid.AllowUserToAddRows = false;
             grid.ReadOnly = true;
-            grid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
             grid.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            DoFormatGrid(grid, tablename);
         }
+
+        public static void FormatGridForEdit(DataGridView grid, string tablename)
+        {
+            grid.EditMode = DataGridViewEditMode.EditOnEnter;
+            DoFormatGrid(grid, tablename);
+        }
+        private static void DoFormatGrid(DataGridView grid, string tablename)
+        {
+            grid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            grid.RowHeadersWidth = 25;
+            string pkname = tablename + "id";
+            foreach (DataGridViewColumn c in grid.Columns)
+            {
+                if (c.Name.EndsWith("Id"))
+                {
+                    c.Visible = false;
+                }
+            }
+            if (grid.Columns.Count > 0 && grid.Columns.Contains(pkname))
+            {
+                grid.Columns[pkname].Visible = false;
+            }
+        }
+
+        public static int GetIdFromGrid(DataGridView grid, int rowindex, string colname)
+        {
+            int id = 0;
+            if (rowindex < grid.Rows.Count && grid.Columns.Contains(colname) && grid.Rows[rowindex].Cells[colname].Value != DBNull.Value)
+            {
+                if (grid.Rows[rowindex].Cells[colname].Value is int)
+                {
+                    id = (int)grid.Rows[rowindex].Cells[colname].Value;
+                }
+            }
+            return id;
+        }
+
+        public static void AddComboBoxToGrid(DataGridView grid, DataTable datasource, string tablename, string displaymember)
+        {
+            DataGridViewComboBoxColumn c = new();
+            c.DataSource = datasource;
+            c.DisplayMember = displaymember;
+            c.ValueMember = tablename + "Id";
+            c.DataPropertyName = c.ValueMember;
+            c.HeaderText = tablename;
+            grid.Columns.Insert(0, c);
+        }
+
+        public static bool IsFormOpen(Type frmType, int pkvalue = 0)
+        {
+            bool exists = false;
+            foreach (Form frm in Application.OpenForms)
+            {
+                int frmpkvalue = 0;
+                if (frm.Tag != null && frm.Tag is int)
+                {
+                    frmpkvalue = (int)frm.Tag;
+                }
+                if (frm.GetType() == frmType && frmpkvalue == pkvalue)
+                {
+                    frm.Activate();
+                    exists = true;
+                    break;
+                }
+            }
+            return exists;
+        }
+
+        public static void SetupNav(ToolStrip ts)
+        {
+            ts.Items.Clear();
+            foreach (Form f in Application.OpenForms)
+            {
+                if (f.IsMdiContainer == false)
+                {
+                    ToolStripButton btn = new ToolStripButton();
+                    btn.Text = f.Text;
+                    btn.Tag = f;
+                    btn.Click += Btn_Click;
+                    ts.Items.Add(btn);
+                    ts.Items.Add(new ToolStripSeparator());
+                }
+            }
+        }
+
+        private static void Btn_Click(object? sender, EventArgs e)
+        {
+            if (sender != null && sender is ToolStripButton)
+            {
+                ToolStripButton btn = (ToolStripButton)sender;
+                if (btn.Tag != null && btn.Tag is Form)
+                {
+                    ((Form)btn.Tag).Activate();
+                }
+            }
+        }
+
+
+        //End Class
     }
 }
